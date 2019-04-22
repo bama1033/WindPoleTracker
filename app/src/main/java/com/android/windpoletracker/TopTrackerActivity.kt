8207030c542310d26980dd274745ebce824d0606
Martin Bauer <mba@adorsys.de>
2019-04-22 11:20:13 +0200

    fixed background

--- app/src/main/java/com/android/windpoletracker/TopTrackerActivity.kt
+++ app/src/main/java/com/android/windpoletracker/TopTrackerActivity.kt
@@ -1,21 +1,148 @@
 package com.android.windpoletracker
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_top_tracker.*
+import org.jsoup.Jsoup
 
 class TopTrackerActivity : AppCompatActivity() {
 
+    private lateinit var list: List<TextView>
+    private lateinit var links: MutableList<String>
+    private var counter = 0
+    private var textidcounter = 0
+
+    private val trackerBotKey = "TrackerBot"
+
+
+    private val bottomHistory = arrayListOf<String>(
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_top_tracker)
+        setContentView(R.layout.activity_bottom_tracker)
+        list = listOf(myID4, myID5)
         setSupportActionBar(toolbar)
 
+        checkHistory()
+
         fab.setOnClickListener { view ->
-            //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
+            useSoup()
+        }
+        fab1.setOnClickListener { view ->
+            writeData(myID4)
+            writeData(myID5)
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun checkHistory() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        if (sharedPreferences != null) {
+            counter = sharedPreferences.getInt(trackerBotKey, 0)
+            if (counter.equals(0)) {
+            } else {
+                fillList(counter)
+            }
+        }
+    }
+
+    private fun writeData(myID: TextView) {
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+//        var trackerBotValue: Int
+        var value: String
+        if (sharedPreferences != null) {
+            counter = sharedPreferences.getInt(trackerBotKey, 0)
+            with(sharedPreferences.edit()) {
+                counter += 1
+                val countervalue = counter
+                val key = "ValueBot$counter"
+                value = myID.text.toString()
+                putString(key, value)
+                putInt(trackerBotKey, countervalue)
+                apply()
+            }
+//            trackerBotValue = sharedPreferences.getInt(trackerBotKey, 0)
+            if (counter.equals(0)) {
+            } else {
+//                fillList(trackerBotValue)
+                Toast.makeText(this, "$value has been added", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun fillList(value: Int) {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        recview.apply {
+            for (item: Int in value downTo 1) {
+                if (sharedPreferences != null) {
+                    val value = "ValueBot$item"
+                    bottomHistory.add(sharedPreferences.getString(value, "error"))
+                }
+            }
+            layoutManager = LinearLayoutManager(context)
+            adapter = Adapter(bottomHistory)
+        }
+    }
+
+
+    fun useSoup() {
+        try {
+            doAsync {
+                var doc = Jsoup.connect("http://192.168.1.3/showvalue.htm").timeout(5000).get()
+                links = doc
+                    .select("tr")
+                    .eachText()
+//                .attr("href")
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    fun taskDone() {
+        runOnUiThread {
+            for (e in links) {
+                when (counter) {
+                    in 5..6 -> {
+                        var x = e.substring(5, e.length)
+                        list[textidcounter].text = x
+//                        list[textidcounter].text = e
+                        textidcounter++
+                    }
+                }
+                counter++
+            }
+            counter = 0
+            textidcounter = 0
+        }
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class doAsync(val handler: () -> Unit) : AsyncTask<Void, Void, Void>() {
+        init {
+            execute()
+        }
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            handler()
+            return null
+        }
+
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            taskDone()
+        }
+    }
 }
+
+
